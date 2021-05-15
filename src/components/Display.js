91c00a67a6b9bb9517884d991156013cfb64a5bf
Your Name <you@example.com>
2021-05-15 13:39:23 +0530

    update

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,4 +1,3 @@
-import { faBorderNone } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import { Link } from "react-router-dom";
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 
